ee1bf05079234ef40fd982b145dae5f52fb75696
kevin nadarajah <kevin.nadarajah@epitech.eu>
2022-05-31 17:50:48 +0200

    [MOD]css form

--- front-app/src/connexion/index.js
+++ front-app/src/connexion/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './index.css'
+import {useHistory} from 'react-router-dom'
 
 export default function Connexion() {
     return (

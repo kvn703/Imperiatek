e2ccbb4939f9e18fe6109e8768918ecf1f058b05
kevin nadarajah <kevin.nadarajah@epitech.eu>
2022-05-31 09:17:29 +0200

    [MOD]Models

--- test/polls/models.py
+++ test/polls/models.py
@@ -1,16 +1,17 @@
 import email
 from django.db import models
+import mysql.connector
 
 # Create your models here.
 
 class User(models.ModeL):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField()
+    u_name = models.CharField(max_length=50)
+    u_surname = models.CharField(max_length=50)
+    u_email = models.EmailField()
+    u_password = models.CharField()
 
 class Booking(models.ModeL):
     date = models.DateField()
-    start = models.CharField()
-    end = models.CharField()
-    name = models.ManyToManyField(User)
+    start = models.DateField()
+    end = models.DateField()
+    name = models.CharField()
